8b62f995bd0f09f7307475542b13ecf20faa802e
Vijay-train <53718047+Vijay-train@users.noreply.github.com>
2023-03-09 09:38:25 +0530

    Updated to NG

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Azure!');
+  res.send('Hello, NG!');
 });
 
 const PORT = process.env.PORT || 3000;
