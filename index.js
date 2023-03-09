131a569d867468ecbf5a02a45f0d77227837336c
Vijay-train <53718047+Vijay-train@users.noreply.github.com>
2023-03-09 09:58:47 +0530

    Additional changes with CD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, NG with CI workflow!');
+  res.send('Hello, NG with CI workflow, now with CD!');
 });
 
 const PORT = process.env.PORT || 3000;
